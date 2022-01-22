39342b32183b52b8d4ec9077f40adccfde98db9a
EduardoGrangeiro2002 <eduatdoaugusto503@gmail.com>
2022-01-22 12:25:43 -0300

    Docker configurado

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,26 +1,25 @@
-import  Express, { NextFunction, Request, Response }  from "express";
+import Express, { NextFunction, Request, Response } from "express";
 
-import "express-async-errors"
+import "express-async-errors";
 
-import { appError } from "./Error/AppError"
+import { appError } from "./Error/AppError";
 
-const app = Express()
+const app = Express();
 
 const PORT = 3333;
-
-
-app.use(Express.json())
+app.use(Express.json());
 
 app.use(
-    (err: Error, request: Request, response: Response, next: NextFunction) => {
-        if(err instanceof appError) {
-            return response.status(err.statusCode).json({message: err.message})
-        }
-        return response.status(500).json({
-            status:  "error",
-            message: `Internal server error ${err.message}`
-        });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof appError) {
+      return response.status(err.statusCode).json({ message: err.message });
     }
-)
+    return response.status(500).json({
+      status: "'error'",
+      message: `Internal server error ${err.message}`,
+    });
+  },
+);
 
-app.listen(PORT, () => console.log(`Server is running in PORT: ${PORT}`) )
+app.listen(PORT, () => console.log(`Server is running in PORT: ${PORT}`));
