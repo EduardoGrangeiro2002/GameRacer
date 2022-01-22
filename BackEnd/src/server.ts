f292a93f8d0a72bce4614d7f2e7b2e3f25bea505
EduardoGrangeiro2002 <eduatdoaugusto503@gmail.com>
2022-01-22 10:57:37 -0300

    Server iniciado e arquivo de erro criado

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,3 +1,26 @@
-import  Express  from "express";
+import  Express, { NextFunction, Request, Response }  from "express";
+
+import "express-async-errors"
+
+import { appError } from "./Error/AppError"
 
 const app = Express()
+
+const PORT = 3333;
+
+
+app.use(Express.json())
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if(err instanceof appError) {
+            return response.status(err.statusCode).json({message: err.message})
+        }
+        return response.status(500).json({
+            status:  "error",
+            message: `Internal server error ${err.message}`
+        });
+    }
+)
+
+app.listen(PORT, () => console.log(`Server is running in PORT: ${PORT}`) )
