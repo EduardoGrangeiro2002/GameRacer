2356efe2011c97578a72acc8db22ed126e61c687
EduardoGrangeiro2002 <eduatdoaugusto503@gmail.com>
2022-02-02 16:38:23 -0300

    adicionado request sessions a pagina

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-undef */
+
+const sessions = {
+  URL: {
+    sessions: "http://192.168.1.8:3333/sessions",
+  },
+
+  login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const loginSession = {
+      username,
+      password,
+    };
+
+    const options = {
+      method: "POST",
+      headers: new Headers({ "Content-type": "application/json" }),
+      body: JSON.stringify(loginSession),
+    };
+
+    fetch(this.URL.sessions, options)
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        this.error(json);
+        return json;
+      });
+  },
+
+  error(object) {
+    const messageError = document.getElementById("error");
+
+    if (!object.message) {
+      messageError.innerHTML = "";
+
+      checkLogin(object);
+
+      return;
+    }
+    messageError.innerHTML = object.message;
+  },
+
+  checkLogin(object) {
+    if (!object.token) {
+      throw new Error("Usuário não logado");
+    }
+  },
+};
+
+export { sessions };
